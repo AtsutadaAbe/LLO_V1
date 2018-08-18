6aa8364b6a85732676dc40fdcf4e11393b37f79b
AtsutadaAbe <a.a.bene19751206u@gmail.com>
2018-08-19 00:43:46 +0900

    8月18日更新

--- LLO_V1/InputValueViewController.swift
+++ LLO_V1/InputValueViewController.swift
@@ -33,6 +33,91 @@ class InputValueViewController: UIViewController {
             self.radio.insertSegment(withTitle: "月に3回以下", at: 2, animated: true)
             self.radio.insertSegment(withTitle: "ほとんど飲まない", at: 3, animated: true)
         }
+        else if(itemName == "脂っぽいものが好き"){
+            self.slider.isHidden = true
+            self.radio.removeAllSegments()
+            self.radio.insertSegment(withTitle: "はい", at: 0, animated: true)
+            self.radio.insertSegment(withTitle: "いいえ", at: 1, animated: true)
+        }
+        else if(itemName == "近くのものを見る時、ピントがあいにくい"){
+            self.slider.isHidden = true
+            self.radio.removeAllSegments()
+            self.radio.insertSegment(withTitle: "はい", at: 0, animated: true)
+            self.radio.insertSegment(withTitle: "いいえ", at:1, animated: true)
+        }
+        else if(itemName == "パソコンや新聞などで目を酷使している"){
+            self.slider.isHidden = true
+            self.radio.removeAllSegments()
+            self.radio.insertSegment(withTitle: "はい", at: 0, animated: true)
+            self.radio.insertSegment(withTitle: "いいえ", at: 1, animated: true)
+        }
+        else if(itemName == "小さな文字をストレスに感じるようになった"){
+            self.slider.isHidden = true
+            self.radio.removeAllSegments()
+            self.radio.insertSegment(withTitle: "はい", at: 0, animated: true)
+            self.radio.insertSegment(withTitle: "いいえ", at: 1, animated: true)
+        }
+        else if(itemName == "目の使い過ぎで肩・首筋が凝っている"){
+            self.slider.isHidden = true
+            self.radio.removeAllSegments()
+            self.radio.insertSegment(withTitle: "はい", at: 0, animated: true)
+            self.radio.insertSegment(withTitle: "いいえ", at: 1, animated: true)
+        }
+        else if(itemName == "毎日スッキリ目覚められる"){
+            self.slider.isHidden = true
+            self.radio.removeAllSegments()
+            self.radio.insertSegment(withTitle: "はい", at: 0, animated: true)
+            self.radio.insertSegment(withTitle: "いいえ", at: 1, animated: true)
+        }
+        else if(itemName == "睡眠時間が短いと感じている"){
+            self.slider.isHidden = true
+            self.radio.removeAllSegments()
+            self.radio.insertSegment(withTitle: "はい", at: 0, animated: true)
+            self.radio.insertSegment(withTitle: "いいえ", at: 1, animated: true)
+        }
+        else if(itemName == "日中の集中力が低下していると感じる"){
+            self.slider.isHidden = true
+            self.radio.removeAllSegments()
+            self.radio.insertSegment(withTitle: "はい", at: 0, animated: true)
+            self.radio.insertSegment(withTitle: "いいえ", at: 1, animated: true)
+        }
+        else if(itemName == "お米、パスタ、パンなど炭水化物を食事で取りすぎている傾向にある"){
+            self.slider.isHidden = true
+            self.radio.removeAllSegments()
+            self.radio.insertSegment(withTitle: "はい", at: 0, animated: true)
+            self.radio.insertSegment(withTitle: "いいえ", at: 1, animated: true)
+        }
+        else if(itemName == "汗をかいてそのまま放置することが多い"){
+            self.slider.isHidden = true
+            self.radio.removeAllSegments()
+            self.radio.insertSegment(withTitle: "はい", at: 0, animated: true)
+            self.radio.insertSegment(withTitle: "いいえ", at: 1, animated: true)
+        }
+        else if(itemName == "睡眠時間が短いと感じている"){
+            self.slider.isHidden = true
+            self.radio.removeAllSegments()
+            self.radio.insertSegment(withTitle: "はい", at: 0, animated: true)
+            self.radio.insertSegment(withTitle: "いいえ", at: 1, animated: true)
+        }
+        else if(itemName == "便秘がちである"){
+            self.slider.isHidden = true
+            self.radio.removeAllSegments()
+            self.radio.insertSegment(withTitle: "はい", at: 0, animated: true)
+            self.radio.insertSegment(withTitle: "いいえ", at: 1, animated: true)
+        }
+        else if(itemName == "食事の回数や時間が日によってバラバラである"){
+            self.slider.isHidden = true
+            self.radio.removeAllSegments()
+            self.radio.insertSegment(withTitle: "はい", at: 0, animated: true)
+            self.radio.insertSegment(withTitle: "いいえ", at: 1, animated: true)
+        }
+        else if(itemName == "蛋白"){
+            self.slider.isHidden = true
+            self.radio.removeAllSegments()
+            self.radio.insertSegment(withTitle: "+", at: 0, animated: true)
+            self.radio.insertSegment(withTitle: "±", at: 1, animated: true)
+            self.radio.insertSegment(withTitle: "-", at: 2, animated: true)
+        }
         else
         {
             self.radio.isHidden = true
